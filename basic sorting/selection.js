c22d6dd25bd5a1bb4f99b3ffd9f21d258a6218c9
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-27 09:16:26 +0530

    Update selection.js

--- basic sorting/selection.js
+++ basic sorting/selection.js
@@ -1,7 +1,7 @@
 let arr = [3, 9, 52, 1, 5, 11];
 
 for (let i = 0; i < arr.length - 1; i++) {
-    let min = Number.MAX_VALUE;
+    let min = Number.MAX_VALUE;  // A very large number provided by JS 
     let minIdx = 0;
     for (let j = i; j <= arr.length - 1; j++) {
         if (arr[j] < min) {
