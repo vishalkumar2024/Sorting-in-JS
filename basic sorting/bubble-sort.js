831071d5217a4ff0a23ff43c7cc71aa046bad106
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-30 09:00:40 +0530

    added a function

--- basic sorting/bubble-sort.js
+++ basic sorting/bubble-sort.js
@@ -1,16 +1,18 @@
 //----------  Brute Force approach  ----------//
 let arr = [52, 24, 53, 2, 11];
-
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length-1 - i; j++) {
-        if (arr[j] > arr[j + 1]) {
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-        }
-    }
+function BubbleSort(arr){
+   for (let i = 0; i < arr.length; i++) {
+       for (let j = 0; j < arr.length-1 - i; j++) {
+           if (arr[j] > arr[j + 1]) {
+               let temp = arr[j];
+               arr[j] = arr[j + 1];
+               arr[j + 1] = temp;
+           }
+       }
+   }
 }
-console.log(arr);
+let arr = [52, 24, 53, 2, 11];
+console.log(BubbleSort(arr));
 
 //---------- Optimise Code  ----------// 
 
