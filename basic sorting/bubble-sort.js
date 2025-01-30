d7c2ac1f0ae30e947dd55ccba0c7a849a12add71
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-30 08:57:31 +0530

    Update bubble-sort.js

--- basic sorting/bubble-sort.js
+++ basic sorting/bubble-sort.js
@@ -14,18 +14,20 @@ console.log(arr);
 
 //---------- Optimise Code  ----------// 
 
-let arr = [52, 24, 53, 2, 11];
 
-for (let i = 0; i < arr.length; i++) {
-    bool = true;
-    for (let j = 0; j < arr.length - 1 - i; j++) {
-        if (arr[j] > arr[j + 1]) {
-            let temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-            bool = false;
-        }
-    }
-    if (bool == true) break;
+function bubbleSort(arr){
+   for (let i = 0; i < arr.length; i++) {
+       bool = true;
+       for (let j = 0; j < arr.length - 1 - i; j++) {
+           if (arr[j] > arr[j + 1]) {
+               let temp = arr[j];
+               arr[j] = arr[j + 1];
+               arr[j + 1] = temp;
+               bool = false;
+           }
+       }
+       if (bool == true) break;
+     }
 }
-console.log(arr);
+let arr = [52, 24, 53, 2, 11];
+console.log(bubbleSort(arr));
