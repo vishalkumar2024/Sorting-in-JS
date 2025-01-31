d0a7ed7ccba5c5a6192c22583e8992f2b93e34e1
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-31 07:34:21 +0530

    wrapup in a function named insertionSort

--- basic sorting/insertion.js
+++ basic sorting/insertion.js
@@ -1,5 +1,6 @@
 let arr = [5, 4, 3, 2, 1];
 
+function insertionSort(arr){
 for (let i = 1; i <= arr.length; i++) {
     let j = i;
     while (j > 0 && arr[j] < arr[j - 1]) {
@@ -8,5 +9,6 @@ for (let i = 1; i <= arr.length; i++) {
         arr[j - 1] = temp;
         j--;
     }
+}
 }
 console.log(arr);
