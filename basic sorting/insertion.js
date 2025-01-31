b0b79803560c0321487d68fd8a15b66f84a710ac
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-01-31 07:35:13 +0530

    Update insertion.js

--- basic sorting/insertion.js
+++ basic sorting/insertion.js
@@ -1,14 +1,14 @@
 let arr = [5, 4, 3, 2, 1];
 
 function insertionSort(arr){
-for (let i = 1; i <= arr.length; i++) {
-    let j = i;
-    while (j > 0 && arr[j] < arr[j - 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j - 1];
-        arr[j - 1] = temp;
-        j--;
-    }
-}
+   for (let i = 1; i <= arr.length; i++) {
+       let j = i;
+       while (j > 0 && arr[j] < arr[j - 1]) {
+          let temp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+          j--;
+       }
+   }
 }
 console.log(arr);
